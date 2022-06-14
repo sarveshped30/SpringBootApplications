56054d76416ca9e006c1680e45ad32c00c833bf7
sarveshped30 <sarveshpednekar30@gmail.com>
2022-06-14 16:13:47 +0530

    Added: HelloWorld SpringbootApplication

---
+++ java/com/example/HelloWorlDemo/HelloWorlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.HelloWorlDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorlDemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello from bridgelabz");
+		SpringApplication.run(HelloWorlDemoApplication.class, args);
+	}
+}
